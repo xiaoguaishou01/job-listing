013a790518d5151b829448d2fffc89a2d2df5c74
weiyueyan <xiaoguaishou0809@gmail.com>
2017-01-09 15:56:57 +0800

    implement part4-v2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_filter :require_is_admin
   def show
-    @job = job.find(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def index
@@ -23,11 +23,11 @@ class Admin::JobsController < ApplicationController
   end
 
   def edit
-    @job = job.find(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def update
-    @job = job.find(params[:id])
+    @job = Job.find(params[:id])
     if @job.update(job_params)
       redirect_to admin_jobs_path
     else
@@ -46,6 +46,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :descriptionwage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
   end
 end
